addf09ac1c587e8930fda36b7b30c9dcd150a956
IKarbowiak <idzi.karbowiak@gmail.com>
2019-09-17 21:47:53 +0200

    Add test for director and topMovies serializer

---
+++ movie/tests/test_serializers.py
@@ -0,0 +1,77 @@
+from django.test import TestCase
+
+from ..models import Director, Movie
+from ..serializers import DirectorSerializer, TopMoviesSerializer
+
+
+class DirectorSerializerTest(TestCase):
+
+    def test_contains_expected_fields_for_correct_data_with_instance(self):
+        # GIVEN
+        data = {'full_name': 'TestDirector'}
+        director = Director.objects.create(**data)
+
+        # WHEN
+        serializer = DirectorSerializer(instance=director)
+
+        # THEN
+        self.assertDictEqual(serializer.data, data)
+
+    def test_contains_expected_fields_for_correct_data_with_data(self):
+        # GIVEN
+        data = {'full_name': 'TestDirector'}
+
+        # WHEN
+        serializer = DirectorSerializer(data=data)
+
+        # THEN
+        self.assertDictEqual(serializer.data, data)
+
+    def test_errors_fields_for_incorrect_data(self):
+        # GIVEN
+        data = {'name': 'TestDirector'}
+
+        # WHEN
+        serializer = DirectorSerializer(data=data)
+
+        # THEN
+        self.assertFalse(serializer.is_valid())
+        self.assertSetEqual(set(serializer.errors), {'full_name'})
+
+
+class TopMovieSerializerTest(TestCase):
+    def test_contains_expected_fields_for_correct_data(self):
+        # GIVEN
+        data = {'total_comments': 1, 'rank': 1, 'movie_id': 1}
+        movie = Movie.objects.create(titl='TestTitle', year=111, plot='TetPlot', genre='TestGenre')
+        movie.pk = 1
+
+        # WHEN
+        serializer = TopMoviesSerializer(data=data)
+
+        # THEN
+        self.assertDictEqual(serializer.data, data)
+
+    def test_errors_fields_for_incorrect_data(self):
+        # GIVEN
+        data = {'total_comments': 'abc', 'rank': 'xyz', 'movie_id': 1}
+        movie = Movie.objects.create(titl='TestTitle', year=111, plot='TetPlot', genre='TestGenre')
+        movie.pk = 1
+
+        # WHEN
+        serializer = TopMoviesSerializer(data=data)
+
+        # THEN
+        self.assertFalse(serializer.is_valid())
+        self.assertSetEqual(set(serializer.errors), {'total_comments', 'rank'})
+
+    def test_errors_fields_for_no_existing_movie_obj(self):
+        # GIVEN
+        data = {'total_comments': 1, 'rank': 1, 'movie_id': 1}
+
+        # WHEN
+        serializer = TopMoviesSerializer(data=data)
+
+        # THEN
+        self.assertFalse(serializer.is_valid())
+        self.assertSetEqual(set(serializer.errors), {'movie_id'})
