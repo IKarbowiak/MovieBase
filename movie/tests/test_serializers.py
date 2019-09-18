c25ba28f7cc9504be3adcfc3c697887e585fc5f0
karbowia <iga.karbowiak@nokia.com>
2019-09-18 12:21:37 +0200

    Add tests for movie serializer

--- movie/tests/test_serializers.py
+++ movie/tests/test_serializers.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from ..models import Director, Movie
-from ..serializers import DirectorSerializer, TopMoviesSerializer
+from ..serializers import DirectorSerializer, TopMoviesSerializer, MovieSerializer
 
 
 class DirectorSerializerTest(TestCase):
@@ -67,3 +67,65 @@ class TopMovieSerializerTest(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertSetEqual(set(serializer.errors), {'total_comments', 'rank'})
 
+
+class MovieSerializerTest(TestCase):
+    def test_contains_expected_fields_for_correct_data_with_instance(self):
+        # GIVEN
+        data = {'title': 'TestTitle', 'year': 111, 'plot': 'TetPlot', 'genre': 'TestGenre'}
+
+        movie = Movie.objects.create(**data)
+        director = Director.objects.create(full_name='TestName')
+        director.movies.add(movie)
+
+        # WHEN
+        serializer = MovieSerializer(instance=movie)
+
+        # THEN
+        data['directors'] = [{'full_name': director.full_name}]
+        self.assertDictEqual(data, serializer.data)
+
+    def test_contains_expected_fields_for_correct_data_with_data(self):
+        # GIVEN
+        data = {'title': 'TestTitle', 'year': 111, 'plot': 'TetPlot', 'genre': 'TestGenre',
+                'directors': [{'full_name': 'TestDirector'}]}
+
+        # WHEN
+        serializer = MovieSerializer(data=data)
+
+        # THEN
+        self.assertTrue(serializer.is_valid())
+        self.assertDictEqual(data, serializer.data)
+
+    def test_contains_expected_fields_for_incorrect_data(self):
+        # GIVEN
+        data = {'title': 'TestTitle', 'year': 'abc', 'plot': 'TetPlot', 'genre': 'TestGenre',
+                'directors': ['TestDirector']}
+
+        # WHEN
+        serializer = MovieSerializer(data=data)
+
+        # THEN
+        self.assertFalse(serializer.is_valid())
+        self.assertSetEqual(set(serializer.errors), {'year', 'directors'})
+
+    def test_save_for_correct_data(self):
+        # GIVEN
+        director_name = 'TestDirector'
+        data = {'title': 'TestTitle', 'year': 111, 'plot': 'TetPlot', 'genre': 'TestGenre',
+                'directors': [{'full_name': director_name}]}
+
+        # WHEN
+        serializer = MovieSerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
+
+        # THEN
+        movies = Movie.objects.all()
+        self.assertEqual(movies.count(), 1)
+
+        movie = movies[0]
+        self.assertEqual(movie.title, data['title'])
+        self.assertEqual(movie.year, data['year'])
+        self.assertEqual(movie.plot, data['plot'])
+        self.assertEqual(movie.genre, data['genre'])
+        self.assertSetEqual({director.full_name for director in movie.directors.all()}, {director_name})
